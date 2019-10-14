bfa00ffe80e2d7ceaf10d09393c5567f5f36013b
Mei Sasaki <october.sky.2009@gmail.com>
2019-10-14 13:05:04 +0900

    よりよい実装に

--- trie/Trie.java
+++ trie/Trie.java
@@ -27,22 +27,14 @@ public class Trie {
         List<Map<Character, Integer>> trie = new ArrayList<Map<Character, Integer>>();
         int maxNode = 1;
         int n = patterns.length;
-        int m = 0;
-        for (int i = 0; i < n; i++) {
-            m += patterns[i].length();
-        }
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[m];
-        for (int j = 0; j < m; j++) {
-            adj[j] = new ArrayList<Integer>();
-            trie.add(new HashMap<Character, Integer>());
-        }
+        trie.add(new HashMap<Character, Integer>());
         for (int i = 0; i < n; i++) {
             int node = 0;
             for (int j = 0; j < patterns[i].length(); j++) {
                 Map<Character, Integer> labelIndexMap = trie.get(node);
                 char ch = patterns[i].charAt(j);
                 int dest = -1;
-                for (int ad: adj[node]) {
+                for (int ad: labelIndexMap.values()) {
                     if (labelIndexMap.containsKey(ch)) {
                         if (labelIndexMap.get(ch) == ad) {
                             dest = ad;
@@ -53,11 +45,11 @@ public class Trie {
                 if (dest != -1) {
                     node = dest;
                 } else {
-                    adj[node].add(maxNode);
                     Map<Character, Integer> localMap = trie.get(node);
                     localMap.put(ch, maxNode);
                     trie.set(node, localMap);
                     node = maxNode;
+                    trie.add(new HashMap<Character, Integer>());
                     maxNode += 1;
                 }
             }
