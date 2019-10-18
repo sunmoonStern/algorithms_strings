2002bf0820667927f2fedefaee2fbb8ac130339b
Mei Sasaki <october.sky.2009@gmail.com>
2019-10-18 13:03:58 +0900

    2問目がtime limit exceeded (Time used: 6.00/3.00)

--- week2/bwtinverse/InverseBWT.java
+++ week2/bwtinverse/InverseBWT.java
@@ -25,10 +25,36 @@ public class InverseBWT {
 
     String inverseBWT(String bwt) {
         StringBuilder result = new StringBuilder();
-
-        // write your code here
-
+        char[] chars = bwt.toCharArray();
+        Arrays.sort(chars);
+        int n = bwt.length();
+        String[] texts = new String[n];
+        for (int i = 0; i < n; i++) {
+            texts[i] = String.valueOf(chars[i]);
+        }
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n; j++) {
+                texts[j] = String.valueOf(bwt.charAt(j)) + texts[j];
+            }
+            texts = sortElems(texts);
+        }
+        result.append(texts[0].substring(1, n));
+        result.append(texts[0].substring(0, 1));
         return result.toString();
+    }
+
+    String[] sortElems(String[] texts) {
+        int n = texts.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (texts[i].compareTo(texts[j]) > 0) {
+                    String tmp = texts[j];
+                    texts[j] = texts[i];
+                    texts[i] = tmp;
+                }
+            }
+        }
+        return texts;
     }
 
     static public void main(String[] args) throws IOException {
