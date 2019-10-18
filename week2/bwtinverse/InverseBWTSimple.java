f12b7e97bb34a84070eec82a6322db0de9720136
Mei Sasaki <mesasaki@yahoo-corp.jp>
2019-10-18 15:52:08 +0900

    まだtime limit exceeded

---
+++ week2/bwtinverse/InverseBWTSimple.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class InverseBWT {
+    class FastScanner {
+        StringTokenizer tok = new StringTokenizer("");
+        BufferedReader in;
+
+        FastScanner() {
+            in = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() throws IOException {
+            while (!tok.hasMoreElements())
+                tok = new StringTokenizer(in.readLine());
+            return tok.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    String inverseBWT(String bwt) {
+        StringBuilder result = new StringBuilder();
+        char[] chars = bwt.toCharArray();
+        Arrays.sort(chars);
+        int n = bwt.length();
+        String[] texts = new String[n];
+        for (int i = 0; i < n; i++) {
+            texts[i] = String.valueOf(chars[i]);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.setLength(0);
+                sb.append(String.valueOf(bwt.charAt(j)));
+                sb.append(texts[j]);
+                texts[j] = sb.toString();
+            }
+            texts = sortElems(texts);
+        }
+        result.append(texts[0].substring(1, n));
+        result.append(texts[0].substring(0, 1));
+        return result.toString();
+    }
+
+    String[] sortElems(String[] texts) {
+        int n = texts.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (texts[i].compareTo(texts[j]) > 0) {
+                    String tmp = texts[j];
+                    texts[j] = texts[i];
+                    texts[i] = tmp;
+                }
+            }
+        }
+        return texts;
+    }
+
+    static public void main(String[] args) throws IOException {
+        new InverseBWT().run();
+    }
+
+    public void run() throws IOException {
+        FastScanner scanner = new FastScanner();
+        String bwt = scanner.next();
+        System.out.println(inverseBWT(bwt));
+    }
+}
